939fe03a9e53316bc931d6c5bdb848c3ed322719
shshwdr <cs.vivian04@gmail.com>
2018-12-10 01:49:01 -0800

    draw a map of hex tiles

---
+++ Assets/Hex.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///  defines the grid position, world space position,size, neighbours, etc. of a hex tile
+///  ref: https://www.redblobgames.com/grids/hexagons/
+/// </summary>
+public class Hex
+{
+    // Q+R+S = 0
+    public int Q; // col
+    public int R; // row
+    public int S; //S = -(Q+R)
+
+    static readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(2) / 2;
+
+    public Hex (int q,int r)
+    {
+        Q = q;
+        R = r;
+        S = -(q + r);
+    }
+    /// <summary>
+    /// return the world space position of the hex
+    /// 
+    /// width
+    ///   /\ 
+    ///  |  |  height
+    ///   \/
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 Position()
+    {
+        float radius = 1f;//distance from center to pointy end of hex
+        float height = radius * 2;
+        float width = WIDTH_MULTIPLIER * height;
+
+        float horiz = width;
+        float vert = height * 0.75f;
+
+        return new Vector3(
+            horiz * (Q+R/2f), //each even row, we need to have another half movement horizontally
+            0,
+            vert * R);
+    }
+}
---
+++ Assets/HexMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    public int mapSize;
+    public GameObject hexPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        for(int col = 0; col < mapSize; col++)
+        {
+            for (int row = 0; row < mapSize; row++)
+            {
+                Hex hex = new Hex(col, row);
+                Instantiate(hexPrefab, hex.Position(), Quaternion.identity, transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
